49eb026cc618d0a00fbc48e8ffae8358a1c23cec
Bambiaxl <254342628+brvanritter-svg@users.noreply.github.com>
2026-04-29 15:01:18 -0300

    Refactor book addition UI and functionality with cover upload support

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 const addBookWindow = document.querySelector(".addbook");
-const initialAddBookWindow = addBookWindow.innerHTML;
+const initialBookWindow = document.querySelector(".initialAddBook");
 const addBookButton = document.querySelector("#addbook");
 
 
 const myLibrary = [];
+let file = [];
 
-function makeBook(title,author,pages, year,read) {
+function makeBook(title,author,pages, year,read,uniqueid) {
     
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.year = year;
-    this.read = read;
+    if (read == null) {
+        return this.read = false;
+    } else {
+        return this.read = true;
+    };
+    this.uniqueid = uniqueid;
     this.info = () => console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read === true ? "done reading" : "not read yet"}`)
 }
 
-function reset() {
-    return addBookWindow.innerHTML = initialAddBookWindow;
+function addBookCard () {
+    const bookWindow = document.querySelector(".books");
+    const infoWindow = document.querySelector(".info");
+
+    myLibrary.forEach(element => {
+        const bookCard = document.createElement("div");
+        bookCard.setAttribute("class","book-card");
+
+        const cover = document.createElement("img");
+        cover.src = URL.createObjectURL(file.at(-1));
+        bookCard.append(cover);
+        bookWindow.append(bookCard);
+    });
 }
+
 
 function addBookToLibrary () {
 
 // Clear div
-
-    addBookWindow.replaceChildren();
+    initialBookWindow.style.display = "none";
+    
+    
 
 // Create form
 
@@ -78,6 +97,33 @@ function addBookToLibrary () {
     yearInput.setAttribute("name","year");
     yearInput.setAttribute("required","");
 
+    // Cover
+
+    const coverLabel = document.createElement("label");
+    coverLabel.setAttribute("for","cover");
+    coverLabel.textContent = ("Cover")
+
+    const coverInput = document.createElement("input");
+    coverInput.setAttribute("type","file");
+    coverInput.setAttribute("id","cover");
+    coverInput.setAttribute("name","cover");
+    coverInput.setAttribute("accept","image/*");
+
+    const coverSpan = document.createElement("span");
+    coverSpan.textContent = "Selected: "
+    coverSpan.setAttribute("class","output");
+
+    coverInput.addEventListener("change", () => {
+
+        const cover = document.querySelector("#cover");
+
+        for (const file of cover.files){
+            let output = document.querySelector(".output");
+            output.textContent = "Selected:"
+            output.innerText += ` ${file.name}`
+        }
+    })
+    
     // Read
 
     const readLabel = document.createElement("label");
@@ -88,6 +134,7 @@ function addBookToLibrary () {
     readInput.setAttribute("type","checkbox");
     readInput.setAttribute("id","read");
     readInput.setAttribute("name","read");
+    readInput.setAttribute("checked","");
 
     // Back button
 
@@ -109,7 +156,7 @@ function addBookToLibrary () {
 
     formButtons.append(backButton,submitButton)
 
-    form.append(titleLabel,titleInput,authorLabel,authorInput,pageLabel,pageInput,yearLabel,yearInput,readLabel,readInput,formButtons);
+    form.append(titleLabel,titleInput,authorLabel,authorInput,pageLabel,pageInput,yearLabel,yearInput,coverLabel,coverInput,coverSpan,readLabel,readInput,formButtons);
     addBookWindow.append(form)
 
     submitButton.addEventListener("click",(event) => {
@@ -119,19 +166,41 @@ function addBookToLibrary () {
         if (form.reportValidity()) {
             const formData = new FormData(form);
         
+            const uniqueid = crypto.randomUUID();
             let title = formData.get("title");
             let author = formData.get("author"); 
             let pages = formData.get("pages");
             let year = formData.get("year"); 
-            let read = formData.get("read"); 
+            let read = formData.get("read");
+            
 
-            newBook = new makeBook(title,author,pages,year,read);
-            console.log(newBook.author)
-            return [myLibrary.push(newBook),reset];
+            const cover = document.querySelector("#cover");
+
+            form.setAttribute("class","form-close")
+
+            newBook = new makeBook(title,author,pages,year,read,uniqueid);
+            
+            form.addEventListener("transitionend",()=>{
+                return [
+                file.push(cover.files[0]),
+                myLibrary.push(newBook),
+                addBookCard(),
+                form.style.display = "none",
+                initialBookWindow.style.display = ""
+                ];
+            })
+            
         }
     
     })
-    backButton.addEventListener("click",reset)
+    backButton.addEventListener("click",()=>{
+        form.setAttribute("class","form-close");
+        form.addEventListener("transitionend",()=> {
+            form.style.display = "none",
+            initialBookWindow.style.display = ""
+        })
+    })
 }
+
 
 addBookButton.addEventListener("click",addBookToLibrary);
